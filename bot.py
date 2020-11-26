a928d1b66d0d3ab496ab7ff98bb61d47983a845d
ignBane <70895936+ignBane@users.noreply.github.com>
2020-11-25 20:41:12 -0500

    Fixed All Requests Problems :)

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import requests, json, discord, datetime, asyncio
+import requests, json, discord, datetime, asyncio, aiohttp
 from discord.ext import commands
 from discord.ext import tasks
 from discord.utils import get
@@ -15,130 +15,167 @@ else:
 
 bot = commands.Bot(command_prefix=config['prefix'])
 
+@bot.command()
+async def aes(ctx):
+  r = requests.get('https://benbotfn.tk/api/v1/aes')
+  rr = r.json()
+
+  mainkey = rr['mainKey']
+  version = rr['version']
+  embed = discord.Embed(title=f"{version} Aes", description="")
+  embed.add_field(name='MainKey', value=f'``{mainkey}``', inline=False)
+  for sub_dict in rr['dynamicKeys']:
+      embed.add_field(name=sub_dict.split('FortniteGame/Content/Paks/')[1].strip().replace('"', ''), value=f"``{rr['dynamicKeys'][sub_dict]}``", inline=False)
+      embed.set_footer(text=f'{ctx.guild.name}', icon_url=f'{ctx.guild.icon_url}')
+  await ctx.send(embed=embed)
+
 @tasks.loop(seconds=10)
 async def taskbrnews():
     with open('Saves/news.json', 'r') as file:
         old = json.load(file)
     if apikey == True:
-        response=requests.get(
-            'https://fortnite-api.com/v2/news/br',
-            headers={"Authorization":config["api-key"]}
-        ).json()
-        if response != old:
-            channel = bot.get_channel(config['news-channel'])
-            embed=discord.Embed(
-                title='Br News',
-            )
-            embed.set_image(url=response['data']['image'])
-            await channel.send(embed=embed)
-        with open('Saves/news.json', 'w') as file:
-            json.dump(response, file, indent=3)
+        async with aiohttp.ClientSession() as session:
+            async with session.get("https://fortnite-api.com/v2/news/br", headers={"Authorization":config["api-key"]}) as r:
+                response = await r.json()
+                if response != old:
+                    channel = bot.get_channel(config['news-channel'])
+                    embed=discord.Embed(
+                        title='Br News',
+                    )
+                    embed.set_image(url=response['data']['image'])
+                    await channel.send(embed=embed)
+                with open('Saves/news.json', 'w') as file:
+                    json.dump(response, file, indent=3)
     else:
-        response=requests.get('https://fortnite-api.com/v2/news/br').json()
-        if response != old:
-            channel = bot.get_channel(config['news-channel'])
-            embed=discord.Embed(
-                title='Br News',
-            )
-            embed.set_image(url=response['data']['image'])
-            await channel.send(embed=embed)
-        with open('Saves/news.json', 'w') as file:
-            json.dump(response, file, indent=3)
+        async with aiohttp.ClientSession() as session:
+            async with session.get("https://fortnite-api.com/v2/news/br") as r:
+                response = await r.json()
+                if response != old:
+                    channel = bot.get_channel(config['news-channel'])
+                    embed=discord.Embed(
+                        title='Br News',
+                    )
+                    embed.set_image(url=response['data']['image'])
+                    await channel.send(embed=embed)
+                with open('Saves/news.json', 'w') as file:
+                    json.dump(response, file, indent=3)
 
 @tasks.loop(seconds=10)
 async def autoshopbr():
     with open('Saves/shop.json', 'r') as file:
         old = json.load(file)
-    response=requests.get('https://api.peely.de/v1/shop').json()
-    if response != old:
-        channel = bot.get_channel(config['shop-channel'])
-        await channel.send(response['time'])
-        await channel.send(response['uniqueurl'])
-    with open('Saves/shop.json', 'w') as file:
-        json.dump(response, file, indent=3)
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get("https://api.peely.de/v1/shop") as r:
+                response = await r.json()
+                if response != old:
+                    channel = bot.get_channel(config['shop-channel'])
+                    await channel.send(response['time'])
+                    await channel.send(response['uniqueurl'])
+                with open('Saves/shop.json', 'w') as file:
+                    json.dump(response, file, indent=3)
+    except:
+        async with aiohttp.ClientSession() as session:
+            async with session.get("https://api.peely.de/v1/shop") as r:
+                response = await r.json()
+                if response != old:
+                    channel = bot.get_channel(config['shop-channel'])
+                    await channel.send(response['time'])
+                    await channel.send(response['uniqueurl'])
+                with open('Saves/shop.json', 'w') as file:
+                    json.dump(response, file, indent=3)
 
 @tasks.loop(seconds=10)
 async def autobuild():
     with open('Saves/build.json', 'r') as file:
         old = json.load(file)
-    status=requests.get(
-        'https://benbotfn.tk/api/v1/status'
-    ).json()
-    if status != old:
-        await bot.change_presence(activity=discord.Game(name=f"{status['currentFortniteVersion']}"))
-        response = requests.get(
-            'https://benbotfn.tk/api/v1/aes'
-        ).json()
-        embed=discord.Embed(
-            title="New Build Detected", 
-            color=0xff0a0a
-        )
-        channel = bot.get_channel(config["build-channel"])
-        pak = 0
-        for sub_dict in response['dynamicKeys']:
-            pak += 1
-        embed.add_field(name=f"BuildVersion", value=f"{response['version']}", inline=True)
-        if pak == 0:
-            embed.add_field(name="Pak Files", value=f"Unkown", inline=True)
-        else:
-            embed.add_field(name="Pak Files", value=f"{pak}", inline=True)
-        await channel.send(embed=embed)
-    with open('Saves/build.json', 'w') as file:
-        json.dump(status, file, indent=3)
+    async with aiohttp.ClientSession() as session:
+        async with session.get("https://benbotfn.tk/api/v1/status") as statuss:
+            status = await statuss.json() 
+            if status != old:
+                await bot.change_presence(activity=discord.Game(name=f"{status['currentFortniteVersion']}"))
+                async with session.get("https://benbotfn.tk/api/v1/aes") as response:
+                    response = await response.json()
+                    embed=discord.Embed(
+                        title="Fortnite Static Key Update Detected", 
+                        color=0xff0a0a
+                    )
+                    channel = bot.get_channel(config["build-channel"])
+                    embed.add_field(name=f"Build", value=f"{response['version']}", inline=False)
+                    embed.add_field(name="AES", value=f"{response['mainKey']}", inline=False)
+                    await channel.send(embed=embed)
+            with open('Saves/build.json', 'w') as file:
+                json.dump(status, file, indent=3)
 
 @tasks.loop(seconds=10)
 async def autotournament():
-    try:
-        with open('Saves/tournament.json', 'r') as file:
-            Cached = json.load(file)
-        data = requests.get(
-            f'https://api.peely.de/v1/tournaments')
-        new = data.json()
-        if data.status_code != 200:
-            return
-    except:
-        return
-    if new["data"]["tournaments"] != Cached["data"]["tournaments"]:
-        for i in new["data"]["tournaments"]:
-            if i not in Cached["data"]["tournaments"]:
-                name = i["name"]
-                short_description = i["description"]
-                image = i['image']
-                try:
-                    channel = bot.get_channel(config["tournamnet-channel"])
-                    embed=discord.Embed(title=f'{name}', description=f'{short_description}')
-                    embed.set_image(url=f'{image}')
-                    await channel.send(embed=embed)
-                except:
-                    channel = bot.get_channel(config["tournamnet-channel"])
-                    embed=discord.Embed(title=f'{name}', description=f'{short_description}')
-                    embed.set_image(url=f'{image}')
-                    await channel.send(embed=embed)
-    with open('Saves/tournament.json', 'w') as file:
-        json.dump(new, file, indent=3)
-    
+    async with aiohttp.ClientSession() as session:
+        async with session.get("https://api.peely.de/v1/tournaments") as data:
+            new = await data.json()
+            try:
+                with open('Saves/tournament.json', 'r') as file:
+                    Cached = json.load(file)
+                if data.status_code != 200:
+                    print(data.status_code + ' Requests Received From api.peely.de/v1/tournaments')
+            except:
+                return
+            if new["data"]["tournaments"] != Cached["data"]["tournaments"]:
+                for i in new["data"]["tournaments"]:
+                    if i not in Cached["data"]["tournaments"]:
+                        name = i["name"]
+                        short_description = i["description"]
+                        image = i['image']
+                        try:
+                            channel = bot.get_channel(config["tournamnet-channel"])
+                            embed=discord.Embed(title=f'{name}', description=f'{short_description}')
+                            embed.set_image(url=f'{image}')
+                            await channel.send(embed=embed)
+                        except:
+                            channel = bot.get_channel(config["tournamnet-channel"])
+                            embed=discord.Embed(title=f'{name}', description=f'{short_description}')
+                            embed.set_image(url=f'{image}')
+                            await channel.send(embed=embed)
+            with open('Saves/tournament.json', 'w') as file:
+                json.dump(new, file, indent=3)
+            
 @tasks.loop(seconds=10)
 async def autonotices():
     with open('Saves/notices.json', 'r') as file:
         old = json.load(file)
-    response=requests.get(
-        'https://api.peely.de/v1/notices'
-    ).json()
-    if response != old:
-        if response['status'] == 200:
-            for sub_dict in response['data']['messages']:
-                channel = bot.get_channel(config['notices-channel'])
-                embed=discord.Embed(title="Notice Changed ", color=0xff0000)
-                embed.add_field(name=f"{sub_dict['title']}", value=f"{sub_dict['body']}", inline=True)
-                await channel.send(embed=embed)
-        else:
-            channel = bot.get_channel(config['notices-channel'])
-            embed=discord.Embed(title="Notice Changed", color=0xff0000)
-            embed.add_field(name=f"???", value=f"???", inline=True)
-            await channel.send(embed=embed)
-    with open('Saves/notices.json', 'w') as file:
-        json.dump(response, file, indent=3)
-
+    async with aiohttp.ClientSession() as session:
+        async with session.get("https://api.peely.de/v1/notices") as data:
+            response=await data.json()
+            try:
+                    if response != old:
+                        if response['status'] == 200:
+                            for sub_dict in response['data']['messages']:
+                                channel = bot.get_channel(config['notices-channel'])
+                                embed=discord.Embed(title="Notice Changed ", color=0xff0000)
+                                embed.add_field(name=f"{sub_dict['title']}", value=f"{sub_dict['body']}", inline=True)
+                                await channel.send(embed=embed)
+                        else:
+                            channel = bot.get_channel(config['notices-channel'])
+                            embed=discord.Embed(title="Notice Changed", color=0xff0000)
+                            embed.add_field(name=f"???", value=f"???", inline=True)
+                            await channel.send(embed=embed)
+                    with open('Saves/notices.json', 'w') as file:
+                        json.dump(response, file, indent=3)
+            except:
+                    if response != old:
+                        if response != old:
+                            if response['status'] == 200:
+                                for sub_dict in response['data']['messages']:
+                                    channel = bot.get_channel(config['notices-channel'])
+                                    embed=discord.Embed(title="Notice Changed ", color=0xff0000)
+                                    embed.add_field(name=f"{sub_dict['title']}", value=f"{sub_dict['body']}", inline=True)
+                                    await channel.send(embed=embed)
+                            else:
+                                channel = bot.get_channel(config['notices-channel'])
+                                embed=discord.Embed(title="Notice Changed", color=0xff0000)
+                                embed.add_field(name=f"???", value=f"???", inline=True)
+                                await channel.send(embed=embed)
+                        with open('Saves/notices.json', 'w') as file:
+                            json.dump(response, file, indent=3)
 @bot.event
 async def on_ready():
     print('Bot Ready/Logged In')
@@ -222,14 +259,25 @@ async def search(ctx, cosnamee):
         embed.add_field(name='Display Asset Path', value=f"``{sub_dict['displayAssetPath']}``", inline=False)
         embed.add_field(name='Definition Path', value=f"``{sub_dict['definitionPath']}``", inline=False)
         embed.set_thumbnail(url=f"https://fortnite-api.com/images/cosmetics/br/{sub_dict['id'].lower()}/icon.png")
-        embed.set_footer(text=f"{bot.user.name} | Made By Bane#9999", icon_url='https://media.fortniteapi.io/images/9322d17f0c849c8d1859753ef237c669/transparent.png')
+        embed.set_footer(text=f"{bot.user.name} | Made By ignBane#1828")
         message = await ctx.send(embed=embed)
   else:
     embed = discord.Embed(color=0xff0f0f)
     embed.add_field(name='Error', value=f"``{rr['error']}``", inline=False)
-    embed.set_footer(text=f"{bot.user.name} | Made By Bane#9999", icon_url='https://media.fortniteapi.io/images/9322d17f0c849c8d1859753ef237c669/transparent.png')
+    embed.set_footer(text=f"{bot.user.name} | Made By ignBane#1828")
     message = await ctx.send(embed=embed)
     await asyncio.sleep(60)
     await message.delete()
+
+@bot.command()
+async def brshop(ctx, aliases=['shop', 'itemshop']):
+    response=requests.get('https://api.peely.de/v1/shop').json()
+    embed=discord.Embed(
+        title=response['time']
+    )
+    embed.set_image(
+        url=response['uniqueurl']
+    )
+    await ctx.send(embed=embed)
 
 bot.run(config["token"], reconnect=True)
